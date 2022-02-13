05c3a2db3a672785a93d37eaca8df9f42b4f7812
mayurrkukreja <mayurkukreja02@gmail.com>
2022-02-13 19:27:09 +0530

    bug fix: file handling

--- lib/dashboard/mainScreen.dart
+++ lib/dashboard/mainScreen.dart
@@ -1,3 +1,4 @@
+import 'package:decode_squad/authentication/WelcomeScreen/welcomeScreen.dart';
 import 'package:decode_squad/referral-system/referral-main.dart';
 import 'package:decode_squad/screens/add_patient_page.dart';
 import 'package:flutter/material.dart';
@@ -24,26 +25,86 @@ class _MainScreenState extends State<MainScreen> {
         color: Color.fromRGBO(63, 63, 63, 1));
 
     return Scaffold(
-            appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text('Patient Form'),
-            ElevatedButton(
-              onPressed: () => {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => AddPatientPage(),
-                  ),
-                  )
-              }, 
-              child: Text('Add',style: TextStyle(fontSize: 20.0)),
-              style: ElevatedButton.styleFrom(primary: Colors.blue
-              ),
+      appBar: AppBar(
+        title:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Text('Home Screen'),
+          ElevatedButton(
+            onPressed: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => AddPatientPage(),
+                ),
               )
-          ]
-        ),
+            },
+            child: Text('Add', style: TextStyle(fontSize: 20.0)),
+            style: ElevatedButton.styleFrom(primary: Colors.blue),
+          )
+        ]),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "Medi-Space",
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 32,
+                fontFamily: "Brand Bold"),
+          ),
+          SizedBox(height: 63),
+          ElevatedButton(
+              onPressed: () => {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => AddPatientPage(),
+                      ),
+                    ),
+                  },
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                      Colors.greenAccent.shade400)),
+              child: Container(
+                  height: 40,
+                  width: 240,
+                  child: Center(
+                    child: Text(
+                      "Add Patient",
+                      style:
+                          TextStyle(fontSize: 24.0, fontFamily: "Brand Bold"),
+                    ),
+                  ))),
+          SizedBox(height: 63),
+          SvgPicture.asset(
+            "images/login.svg",
+            height: size.height * 0.3,
+          ),
+          SizedBox(height: 63),
+          ElevatedButton(
+              onPressed: () => {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => WelcomeScreen(),
+                      ),
+                    )
+                  },
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.blue.shade700)),
+              child: Container(
+                  height: 40,
+                  width: 240,
+                  child: Center(
+                    child: Text(
+                      "Logout",
+                      style:
+                          TextStyle(fontSize: 24.0, fontFamily: "Brand Bold"),
+                    ),
+                  ))),
+        ],
       ),
     );
   }
